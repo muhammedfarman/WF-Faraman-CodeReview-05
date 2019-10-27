b4f7852285f22ed20ccdda8be0591fce4b957c19
Mo Faraman <mofarman102@gmail.com>
2019-10-27 19:30:12 +0100

    search , fav , heart

--- src/script/script.js
+++ src/script/script.js
@@ -51,8 +51,8 @@ function clearmain(){
 $("#male").on("click",function(){
 
 	$("#index").empty();
-	
 	output(males);
+	showfav(males);
 
 });
 $("#female").on("click",function(){
@@ -60,13 +60,16 @@ $("#female").on("click",function(){
 	$("#index").empty();
 	
 	output(females);
+	clearfav()
+	showfav(females);
 
 });
 $("#age").on("click",function(){
 
 	$("#index").empty();
-	
 	output(age);
+	clearfav()
+	showfav(age);
 
 });
 $("#all").on("click",function(){
@@ -74,91 +77,73 @@ $("#all").on("click",function(){
 	$("#index").empty();
 	
 	output(people);
+	clearfav()
+	showfav(people);
 
 });
+$("#sinput").on("change",function(){$("#index").empty();
+result = [] ;
+input = $("#sinput").val();
+result = people.filter(x => x.name.toLowerCase().startsWith(input.toLowerCase()));
+
+output(result);
+
+console.log("sera " ,result);
+})
 
 
 
-
-$("#search").on("click",function(){
-	input = $("#sinput").val();
-	
-	newArr = [];
-
-  console.log ("input",input);
-   
-   newArr = people.filter(x => x.name.toLowerCase().startsWith(input.toLowerCase()));
- 
-   $("#index").empty();
-  output(newArr);
-  	console.log("search ",newArr);
-	})
-	
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fav=[...people];
 
 $(".fa-heart").click(function () {
 	id = $(this).attr("id");
 	v = $(this).attr("value");
-	if (people[id].fav == 0) {
-		people[id].fav = 1;
+	console.log(v);
+	if (v == 0) {
+		$(this).attr("value" ,"1");
+		fav[id].fav = 1;
 	}
-	else if (people[id].fav == 1) {
-		people[id].fav = 0;
+	
+	else if (v == 1) {
+		$(this).attr("value" ,"0");
+		fav[id].fav = 0;
+		
+		
 	}
-	clearfav();
-	showfav();
+	console.log("fav",fav);
+clearfav();
+showfav(fav);
 
-
-	console.log(people);
 	function clearfav() {
 		$("#output").empty();
 
 	}
 
 	function showfav() {
-		x = 0;
+
 		$("#output").empty();
 
-		for (x == 0; x < people.length; x++) {
-			if (people[x].fav == 1) {
+		for (fa of fav) {if(fa.fav==1){
+			  
 				$("#output").append(`<div class=" col-xl-4 col-lg-4  col-md-4 col-sm-12 bg-dark card mb-4 mr-1 item" >
-	 <img class="card-img-top pt-2" src=${people[x].img}  alt=${people[x].name} height="800vh">
+	 <img class="card-img-top pt-2" src=${fa.img}  alt=${fa.name} height="800vh">
 													   
 														  
 							
 	<div class="card-body  text-light rounded-bottom text-center">
 													 
-		 <h4 class="card-title">${people[x].name}</h4>
+		 <h4 class="card-title">${fa.name}</h4>
 														
 	 <hr class="bg-light">
-	<p class="card-text text-white mb-4">${people[x].info}<br>${people[x].dec}</p>
+	<p class="card-text text-white mb-4">${fa.info}<br>${fa.dec}</p>
 	 <!--progress-->
 	<div class="progress">
 	<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
 	 aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 90%"></div>
 	</div>
 	</div>`)
-			};
-		}
+			;
+		};}
 	};
 
 })
